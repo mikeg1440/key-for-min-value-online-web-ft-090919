c5396b671f20f33ca37a35fa290d2b0b57e81f7f
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-11 00:56:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,13 @@
 
 def key_for_min_value(name_hash)
   
-  min_value = ""
+  min_value = 100000
+  min_key = ""
   
   name_hash.each do |key, value|
     
     if value < min_value
-      min_value = key
+      min_key = key
     end
     
   end
